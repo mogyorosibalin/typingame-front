df2a85f45798a1e129528fd61eec3e677a2b5b1b
Balint Mogyorosi <mogyorosibalin@gmail.com>
2019-01-14 14:02:43 +0100

    Updated the input-fields directive to watch the element content on init

--- src/app/shared/directives/input-fields.directive.ts
+++ src/app/shared/directives/input-fields.directive.ts
@@ -1,16 +1,20 @@
-import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appInputFilled]'
 })
-export class InputFilledDirective {
+export class InputFilledDirective implements OnInit {
 
-  @HostBinding('class.filled') isFilled = false;
+  @HostBinding('class.filled') isFilled: boolean;
 
   @HostListener('focusout') focusOut() {
     this.isFilled = this.element.nativeElement.value !== '';
   }
 
   constructor(private element: ElementRef) { }
+
+  ngOnInit() {
+    this.isFilled = this.element.nativeElement.value !== '';
+  }
 
 }
